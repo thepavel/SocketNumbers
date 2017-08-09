0f600df6a67edd521fffe6b27febf088aae1ce03
Pavel Lutskovsky <lutskovp@gmail.com>
2017-08-08 20:04:56 -0700

    initial commit: configuration, basic solution

---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Server
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+
+        static void Main(string[] args)
+        {
+            var configuration = GetConfiguration(args);
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static ServerSettings GetConfiguration(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("config.json")
+                    .AddCommandLine(args);
+            
+            Configuration = builder.Build();
+
+            return new ServerSettings(Configuration.GetSection("Server"));
+        }
+    }
+}
---
+++ Server/ServerSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+namespace Server
+{
+    public class ServerSettings
+    {
+        public ServerSettings() {
+            
+        }
+
+        public ServerSettings(IConfigurationSection configSection) {
+
+            if (configSection != null)
+            {
+                Port = GetIntValue(configSection, "Port");
+                MaxClients = GetIntValue(configSection, "MaxClients");
+                OutputIntervalSeconds = int.Parse(configSection["OutputInterval"]);
+                LogName = configSection["LogName"];
+                TerminateCommand = configSection["TerminateCommand"];
+            }
+        }
+        public int Port { get; set; }
+        public int MaxClients { get; set; }
+        public int OutputIntervalSeconds { get; set; }
+        public string LogName { get; set; }
+        public string TerminateCommand { get; set; }
+
+        public static Dictionary<string, dynamic> DefaultSettings => new Dictionary<string, dynamic>() {
+                    { "Port", 4000},
+
+
+                };
+
+        public static int GetIntValue(IConfigurationSection section, string key) {
+            return section[key] != null ? int.Parse(section[key]) : int.Parse(DefaultSettings[key]);
+        }
+    }
+}
